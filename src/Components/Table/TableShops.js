13f3d7676f497cbc9babfc3257dd62f29aa791bf
Juan <jjfalonso@gmail.com>
2020-05-10 04:34:15 -0300

    TableShops columns added and styling

--- src/Components/Table/TableShops.js
+++ src/Components/Table/TableShops.js
@@ -1,27 +1,26 @@
 import React, { useState, useEffect } from 'react';
-import { Table, Switch, Dropdown, Menu } from 'antd';
+import { Table, Switch, Dropdown, Menu, Button } from 'antd';
 import 'firebase/firestore';
 import firebase from "../../utils/firebase";
-import { toArray } from 'antd/lib/form/util';
-import { DownOutlined } from '@ant-design/icons';
+import { DownOutlined, EditOutlined, DeleteOutlined, SaveOutlined } from '@ant-design/icons';
 
 let db = firebase.firestore();
 
 
-const dataSource = [
-    {
-        key: '1',
-        name: 'Mike',
-        age: 32,
-        address: '10 Downing Street',
-    },
-    {
-        key: '2',
-        name: 'John',
-        age: 42,
-        address: '10 Downing Street',
-    },
-];
+// const dataSource = [
+//     {
+//         key: '1',
+//         name: 'Mike',
+//         age: 32,
+//         address: '10 Downing Street',
+//     },
+//     {
+//         key: '2',
+//         name: 'John',
+//         age: 42,
+//         address: '10 Downing Street',
+//     },
+// ];
 
 const columns = [
     {
@@ -51,10 +50,10 @@ const columns = [
                 }
             >
                 <a className="ant-dropdown-link" onClick={e => e.preventDefault()}>
-                    { type == 1 && 'Envíos' } 
-                    { type == 2 && 'Takeaway' } 
-                    { type == 3 && 'Envíos y Takeaway' } 
-                    <DownOutlined />
+                    {type == 1 && 'Envíos'}
+                    {type == 2 && 'Takeaway'}
+                    {type == 3 && 'Envíos y Takeaway'}
+                    {' '} <DownOutlined />
                 </a>
             </Dropdown>
 
@@ -72,6 +71,14 @@ const columns = [
         title: 'Acciones',
         dataIndex: 'Acciones',
         key: 'Acciones',
+        render: () => (
+            <>
+                {/* <Button type="link" icon={<SaveOutlined />}>Guardar</Button> */}
+                <Button type="link" icon={<EditOutlined />}>Editar</Button>
+                <Button type="link" icon={<DeleteOutlined />}>Eliminar</Button>
+            </>
+
+        )
     },
 ];
 
