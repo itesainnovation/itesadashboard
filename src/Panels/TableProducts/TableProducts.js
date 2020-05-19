437f32a3ae8ea060739e1b25f157b60cb1efb530
JJFAO <jjfalonso@gmail.com>
2020-05-19 01:09:18 -0300

    expandleRow in TableProducts for shops list; EditableTagGroup added to display shops; rc-tween-one dep installed

--- src/Panels/TableProducts/TableProducts.js
+++ src/Panels/TableProducts/TableProducts.js
@@ -4,52 +4,18 @@ import 'firebase/firestore';
 import firebase from "../../utils/firebase";
 import { DownOutlined, EditOutlined, DeleteOutlined, SaveOutlined } from '@ant-design/icons';
 import styles from './tableproducts.module.scss';
+import EditableTagGroup from '../Components/EditableTagGroup/EditableTagGroup';
 
 
-const columns = [
-    {
-        title: 'Nombre',
-        dataIndex: 'name',
-        key: 'name',
-    },
-    {
-        title: 'Precio',
-        dataIndex: 'price',
-        key: 'price',
-        render: (price) => (
-            '$ ' + price
-        )
-    },
-    {
-        title: 'Acciones',
-        dataIndex: 'Acciones',
-        key: 'Acciones',
-        render: () => (
-            <>
-                {false && <Button type="link" icon={<SaveOutlined />}>Guardar</Button>}
-                <Button type="link" icon={<EditOutlined />}>Editar</Button>
-                <Button type="link" icon={<DeleteOutlined />}>Eliminar</Button>
-            </>
-
-        ),
-    },
-    {
-        title: 'Tiendas',
-        dataIndex: 'shops',
-        key: 'shops',
-    },
-];
-
-
-const TableProducts = ({id}) => {
+const TableProducts = ({ userID }) => {
 
     const [products, setProducts] = useState([])
 
     useEffect(() => {
-        if (id) {
+        if (userID) {
             let db = firebase.firestore();
 
-            db.collection('products').where('userID', '==', id)
+            db.collection('products').where('userID', '==', userID)
                 .onSnapshot(function (docs) {
                     let theData = [];
                     docs.forEach(doc => {
@@ -58,22 +24,68 @@ const TableProducts = ({id}) => {
 
                         theData = [...theData, prod]
                     })
-                    console.log( theData );
-                    
+                    console.log(theData);
+
                     setProducts(theData);
                 })
         }
-    }, [id])
+    }, [userID])
+
+
+    const columns = [
+        {
+            title: 'Nombre',
+            dataIndex: 'name',
+            key: 'name',
+        },
+        {
+            title: 'Precio',
+            dataIndex: 'price',
+            key: 'price',
+            render: (price) => (
+                '$ ' + price
+            )
+        },
+        {
+            title: 'Acciones',
+            dataIndex: 'key',
+            key: 'key',
+            render: () => (
+                <>
+                    {false && <Button type="link" icon={<SaveOutlined />}>Guardar</Button>}
+                    <Button type="link" icon={<EditOutlined />}>Editar</Button>
+                    <Button type="link" icon={<DeleteOutlined />}>Eliminar</Button>
+                </>
+
+            ),
+        },
+
+    ];
+
 
     return (
-        <div className="tableProducts">
-            <Spin spinning={!products.length} delay={400}>
-            <Table
-                bordered
-                dataSource={products}
-                columns={columns}
-            />
-        </Spin>
+        <div className={styles.tableContent}>
+            <div className={styles.scrollTable}>
+                <Spin spinning={!products.length} delay={400}>
+                    <Table
+                        bordered
+                        dataSource={products}
+                        columns={columns}
+                        expandable={{
+                            expandedRowRender: record => (<>
+                                <EditableTagGroup /> <p style={{ margin: 0 }}>{record.shops}</p>
+                            </>),
+                            rowExpandable: record => record.shops.length,
+                            expandIconColumnIndex: 4,
+
+                        }}
+                    />
+                </Spin>
+            </div>
+            {/* <Button onClick={handleNew} type="primary" disabled={editable !== ''} className={styles.btnRelative}>
+                <span className={styles.btnIcon}>+</span>
+                Agregar nueva tienda
+            </Button> */}
         </div>
     );
 };
